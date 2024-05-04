f4f60f6ba8b486e1f4930733d305a6aacd9c47d4
Quarrancar <79113873+Quarrancar@users.noreply.github.com>
2024-05-04 15:12:09 +0200

    transcribewizard-logo-and-frontend

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className='flex items-center justify-between gap-4 p-4'>
         <h1 className='font-regular'>Transcribe
-              <span className='text-blue-400 font-bold'>Wizard</span>
+              <span className='font-bold text-blue-400'>Wizard</span>
         </h1>
         <button className='flex items-center gap-2'>
           <p>New</p>
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -2,10 +2,20 @@ import React from 'react'
 
 export default function Homepage() {
   return (
-        <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5'>
+        <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5 text-center'>
             <h1 className='font-regular text-5xl sm:text-6xl'>Transcribe
-              <span className='text-blue-400 font-bold'>Wizard</span>
+              <span className='font-bold text-blue-400'>Wizard</span>
             </h1>
+            <h3 className='font-medium'>Record 
+                <span className='text-blue-400 ml-2 mr-2'>
+                    <i class="fa-solid fa-arrow-right"></i>
+                </span>
+                Transcribe
+                <span className='text-blue-400 ml-2 mr-2'>
+                    <i class="fa-solid fa-arrow-right"></i>
+                </span>
+                Translate
+            </h3>
         </main>
   )
 }
