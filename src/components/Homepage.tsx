f63d4e0f47805708c691ed97f8dc8a2140519c26
Quarrancar <79113873+Quarrancar@users.noreply.github.com>
2024-05-04 11:37:31 +0200

    frontend-changes-and-components

---
+++ src/components/Homepage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Homepage() {
+  return (
+        <main className='flex-1 p-4 flex flex-col gap-3 sm:gap-4 md:gap-5'>
+            <h1 className='font-regular text-5xl sm:text-6xl'>Transcribe
+              <span className='text-blue-400 font-bold'>Wizard</span>
+            </h1>
+        </main>
+  )
+}
